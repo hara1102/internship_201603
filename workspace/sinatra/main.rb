21d6118cae81421cac8f326ef024ab28591b56e5
hara1102 <yamatoharakobe@gmail.com>
2016-03-14 02:06:28 +0000

    画像投稿機能を追加の修正

--- workspace/sinatra/main.rb
+++ workspace/sinatra/main.rb
@@ -4,95 +4,85 @@ require 'active_record'
 require 'pry'
 
 ActiveRecord::Base.establish_connection(
-    "adapter" => "sqlite3",
-    "database" => "./bbs.db"
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
 )
 
 class Comment < ActiveRecord::Base
 end
 
 
-# 静的コンテンツ参照のためのパス設定
-set :public, File.dirname(__FILE__) + '/public'
-
-
 helpers do
-    include Rack::Utils
-    alias_method :h, :escape_html
-
-    #トリップキーを返します
-    def putTripkey(s)
-        @pos =s.index("#")
-        return nil if @pos == nil
-        @tripkey = s.slice(@pos,s.size)
+  include Rack::Utils
+  alias_method :h, :escape_html
+  
+  #トリップキーを返します
+  def put_tripkey(s)
+    @pos =s.index("#")
+    return nil if @pos == nil
+    @tripkey = s.slice(@pos,s.size)
         
-        return @tripkey
-    end
+    return @tripkey
+  end
     
-    #トリップキー以外を返します
-    def sliceComment(s)
-        @pos =s.index("#")
-        return s if @pos == nil
-        @comment = s.slice(0,@pos)
-        return  @comment
-    end
+  #トリップキー以外を返します
+  def slice_comment(s)
+    @pos =s.index("#")
+    return s if @pos == nil
+    @comment = s.slice(0,@pos)
+    return  @comment
+  end
     
-    #トリップキーを変換します
-    def trip10(tripkey)
-        return "◆"  if tripkey == nil
-        
-        @salt = (tripkey + "H.").slice(1, tripkey.size)
-        
-        @salt = @salt.gsub(/[^\.-z]/, ".")
-        @salt = @salt.tr(":;<=>?@[\\]^_`", "ABCDEFGabcdef");
+  #トリップキーを変換します
+  def trip10(tripkey)
+    return "◆"  if tripkey == nil
+    
+    @salt = (tripkey + "H.").slice(1, tripkey.size)
+    @salt = @salt.gsub(/[^\.-z]/, ".")
+    @salt = @salt.tr(":;<=>?@[\\]^_`", "ABCDEFGabcdef");
      
-        # 末尾から10文字取り出す
-        @trip = "◆" + tripkey.crypt(@salt).slice(-10, 10);
-        return @trip
-    end
+    # 末尾から10文字取り出す
+    @trip = "◆" + tripkey.crypt(@salt).slice(-10, 10);
+    return @trip
+  end
 
 
-    # 画像をアップロード
-    def imageLord
-        if params[:file]
-            
-            save_path = "./public/images/#{params[:file][:filename]}"
-            
-	        File.open(save_path, 'wb') do |f|
-	            f.write params[:file][:tempfile].read
-	            @mes = ""
-	        end
-	        
-	        save_path = "./images/#{params[:file][:filename]}"
-            
-	        return save_path 
+  # 画像をアップロード
+  def image_load
+    if params[:file]
+      
+      @save_path = "./public/images/#{ params[:file][:filename] }"
+      
+      File.open(@save_path, 'wb') do |f|
+	    f.write params[:file][:tempfile].read
+	  end
+	  
+	  #カレントディレクトリが[public]になっているのでpathを合わせる
+	  @load_path =  @save_path.gsub("public/", "./")
+      
+	  return @load_path 
 	 
-        else
-	        @mes = "アップロードに失敗しました"
-	        return nil
-        end
-    end    
+    else
+	  return nil
+    end
+  end    
 	    
-    
 end
 
 get '/' do
-    @comments = Comment.order("id desc").all
-    erb:index
+  @comments = Comment.order("id desc").all
+  erb:index
 end
 
 post '/new' do
-    
-    @tripkey = putTripkey(params[:body])
-    
-    params[:body]= sliceComment(params[:body]) +  trip10(@tripkey) 
-    
-    
-    Comment.create({:body => params[:body], :image => imageLord})
-    
-    redirect '/'
+  @tripkey = put_tripkey(params[:body])
+  params[:body]= slice_comment(params[:body]) +  trip10(@tripkey)
+  
+  Comment.create({ body: params[:body], image: image_load })
+
+  redirect '/'
 end
 
 post '/delete' do
-    Comment.find(params[:id]).destroy
+  Comment.find(params[:id]).destroy
 end
